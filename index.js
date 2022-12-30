abb5986d3c79880914da911f60b2408d257859ca
Aku Kettunen <aku@kepit.fi>
2022-12-30 18:29:05 +0200

    added dockerfile and docker-compose

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
       app = express()
       bodyParser = require('body-parser')
+      require("dotenv").config()
 
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
 
-var port = process.env.PORT || 4040;
+var port = process.env.NODE_DOCKER_PORT || 4040;
 
 app.get('/', (req, res) => {
   res.json({ok: 'ok'})
