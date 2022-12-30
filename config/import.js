3607fb6ae896b8fd82fe232881375fb78e88d787
Aku Kettunen <aku@kepit.fi>
2022-12-31 01:14:17 +0200

    implemented reading the csv files in import.js

--- config/import.js
+++ config/import.js
@@ -1,8 +1,20 @@
-const { parse } = require('csv-parse')
+const fs = require('fs')
+const readline = require('readline')
 
 console.log('importing data...')
 
+function parse(fileName) {
+  let counter = 0
 
+  const readStream = fs.createReadStream(fileName)
+  let rl = readline.createInterface({ input: readStream })
+
+  rl.on('line', line => {
+    console.log(line)
+  })
+}
+
+parse('data/trips_1.csv')
 
 // TODO:
-// Implement validating and importing data to db
+// Implement validating and inserting data to db
